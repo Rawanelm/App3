931615e36826bc66b65b73a0f4c20f7c28860969
Rawanelm <relmoghr@uwo.ca>
2022-12-01 11:08:01 -0500

    shield animation

--- Assets/Scripts/GeneratePickup.cs
+++ Assets/Scripts/GeneratePickup.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class GeneratePickup : MonoBehaviour
 {
+
+    //public GameObject shieldPickUp;
+   // public GameObject throwPickUp;
+    //public GameObject shootPickUp;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Instantiate(shieldpickUp, new Vector3(3, 1, -4), Quaternion.identity);
+        //Instantiate(shieldpickUp, new Vector3(-4, 1, -4), Quaternion.identity);
+        //Instantiate(shieldpickUp, new Vector3(22, 1, 4), Quaternion.identity);
+        //Instantiate(shieldpickUp, new Vector3(29, 1, 4), Quaternion.identity);
+
+        //Instantiate(throwPickUp, new Vector3(22, 1, -5), Quaternion.identity);
+        //Instantiate(throwPickUp, new Vector3(29, 1, -5), Quaternion.identity);
+        //Instantiate(throwPickUp, new Vector3(4, 1, 4), Quaternion.identity);
+        //Instantiate(throwPickUp, new Vector3(-4, 1, 4), Quaternion.identity);
+
+        //Instantiate(shootPickUp, new Vector3(22, 1, -5), Quaternion.identity);
+        //Instantiate(shootPickUp, new Vector3(29, 1, -5), Quaternion.identity);
+        //Instantiate(shootPickUp, new Vector3(4, 1, 4), Quaternion.identity);
+        //Instantiate(shootPickUp, new Vector3(-4, 1, 4), Quaternion.identity);
+
     }
 
     // Update is called once per frame
